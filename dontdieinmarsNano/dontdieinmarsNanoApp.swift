3a32e0bb3077c465e9a40ceb917fd4bf37f76509
Rio Ikhsan <mrikhsankamal@gmail.com>
2024-05-04 16:25:55 +0700

    initial commit

---
+++ dontdieinmarsNano/dontdieinmarsNanoApp.swift
@@ -0,0 +1,17 @@
+//
+//  dontdieinmarsNanoApp.swift
+//  dontdieinmarsNano
+//
+//  Created by Rio Ikhsan on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct dontdieinmarsNanoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
